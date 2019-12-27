594b909d0d69c921f3646f9b89c0ea029e370328
sanghakim <sanghakim@KRLT1906053.kr.kworld.kpmg.com>
2019-12-27 18:01:22 +0900

    DefaultRequestToViewNameTranslator 테스트

--- src/main/java/com/updown/spring/IndexController.java
+++ src/main/java/com/updown/spring/IndexController.java
@@ -4,26 +4,30 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class IndexController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String index(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -33,7 +37,16 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "index";
 	}
 	
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+	public ModelAndView home (HttpServletRequest request, HttpServletResponse response) {
+		    
+		    ModelAndView mav = new ModelAndView();
+
+		    return mav;
+	}
+
+
 }
